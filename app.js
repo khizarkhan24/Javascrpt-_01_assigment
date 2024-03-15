5e7670d5d3a5653127c68a9a54046a703b6a350a
Muhammad Khizar Khan <160830258+khizarkhan24@users.noreply.github.com>
2024-03-15 21:07:03 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+// ALERT
+// alert("Error! Please Enter a valid Password");
+
+// alert("Welcome to JS \n Happy Coding");
+
+// alert("hello! i can run through JS my web browser console");
+
+
+
+// VARAIABLES
+// var h = "hello world"
+// alert(h);
+
+// var username = "Muhammad Khizar Khan";
+// alert(username);
+
+// var my_age =  "My age is 21" ;
+// alert(my_age);
+
+
+// var email = "My email is khizar" + "@gmail.com";
+// alert(email);
+
+
+// var a = "smarter way to learn JavaScript";
+// alert(a);
+
+// var  symbol = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬"
+// alert(symbol);
+
+// var text = "yah! I can write HTML cotet through javascript";
+// console.log(text);
+// document.write(text);
+
+// if(localStorage.pageVisitCount) {
+//     localStorage.pageVisitCount = Number(localStorage.pageVisitCount) + 1;
+// } else {
+//     localStorage.pageVisitCount = 1;
+// }
+// alert("You have visited this site " + localStorage.pageVisitCount + " times.");
+
+// var birth_year = 2002
+// alert('My birthday is' + ' ' + birth_year  );
+
+// var visitorName = 'Khizar';
+// var productTittle = 'shoes';
+// var quantity = 11;
+
+// console.log(visitorName + 'ordered' +  quantity + productTittle + 'on khizar.store') ;
+
+// var $_1stVar = "A heading stating “Rules for naming JS variables "  
+// var $_2ndVar = "Variable names can only contain number, $, variables and variables+number";
+// var $_3rdVar = "Variable names can only contain __letters____, ____$__, _letters + number_____ and _$ + letter_____.";
+
+
+// console.log($_1stVar ,  $_2ndVar , $_3rdVar   );
+// document.write( );
+// document.write();
+
+
+
+
+
+// MATHS EXPRESSiON 
+var num;
+document.write("Value after variable declaration is: " + num + "<br>");
+
+num = 5;
+document.write("Initial value: " + num + "<br>");
+
+num++;
+document.write("Value after increment is: " + num + "<br>");
+
+num += 7;
+document.write("Value after addition is: " + num + "<br>");
+
+num--;
+document.write("Value after decrement is: " + num + "<br>");
+
+var remainder = num % 3;
+document.write("The remainder is : " + remainder);
+
